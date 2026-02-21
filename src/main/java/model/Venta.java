88e7885bc5b17b59fdb59d7a40a5485a586283e0
Lorena Soto Zenteno <lsotozenteno@gmail.com>
2026-02-21 00:10:47 -0500

    Implementación del modelo Venta

--- src/main/java/model/Venta.java
+++ src/main/java/model/Venta.java
@@ -4,13 +4,95 @@ package model;
 import java.sql.Timestamp;
 
 public class Venta {
-    private int idVenta;
-    private Timestamp fecha;
-    private int idCliente;
-    private int idUsuario;
-    private double subtotal;
+    private int id;
+    private String tipoComprobante;
+    private String num_comprobante;
+    private Timestamp fecha_venta;
     private double total; 
-    private String métodoPago;
+    private String metodoPago;
+    private double montoRecibido;
+    private double vuelto;
+    private Cliente cliente;
+    private Usuario usuario;
+
+    public Venta(int id, String tipoComprobante, String num_comprobante, Timestamp fecha_venta, double total, String metodoPago, double montoRecibido, double vuelto, Cliente cliente, Usuario usuario) {
+        this.id = id;
+        this.tipoComprobante = tipoComprobante;
+        this.num_comprobante = num_comprobante;
+        this.fecha_venta = fecha_venta;
+        this.total = total;
+        this.metodoPago = metodoPago;
+        this.montoRecibido = montoRecibido;
+        this.vuelto = vuelto;
+        this.cliente = cliente;
+        this.usuario = usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getTipoComprobante() {
+        return tipoComprobante;
+    }
+    public String getNum_comprobante() {
+        return num_comprobante;
+    }
+    public Timestamp getFecha_venta() {
+        return fecha_venta;
+    }
+    public double getTotal() {
+        return total;
+    }
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+    public double getMontoRecibido() {
+        return montoRecibido;
+    }
+    public double getVuelto() {
+        return vuelto;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTipoComprobante(String tipoComprobante) {
+        this.tipoComprobante = tipoComprobante;
+    }
+    public void setNum_comprobante(String num_comprobante) {
+        this.num_comprobante = num_comprobante;
+    }
+    public void setFecha_venta(Timestamp fecha_venta) {
+        this.fecha_venta = fecha_venta;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+    public void setMontoRecibido(double montoRecibido) {
+        this.montoRecibido = montoRecibido;
+    }
+    public void setVuelto(double vuelto) {
+        this.vuelto = vuelto;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+    
+    
     
     
 }
